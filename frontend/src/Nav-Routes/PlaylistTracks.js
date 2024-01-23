c847418b30909e58175edb5b3d5627a258b2dda8
Shahabr99 <sean.rohani9@gmail.com>
2024-01-23 00:06:44 -0800

    add tracks to playlist

--- frontend/src/Nav-Routes/PlaylistTracks.js
+++ frontend/src/Nav-Routes/PlaylistTracks.js
@@ -1,13 +1,14 @@
-import React, {useState, useEffect} from "react";
-
+import React, {useState, useEffect, useContext} from "react";
+import DataContext from "../helpers/DataContext";
 
 function PlaylistTracks() {
   const [playlistTracks, setPlaylistTracks] = useState([]);
+  const { requestTracks } = useContext(DataContext)
 
   useEffect(function getData() {
     async function getPlaylistTracks() {
       try{
-        
+        const result = await requestTracks()
       }catch(err){
         console.error(err)
       }
